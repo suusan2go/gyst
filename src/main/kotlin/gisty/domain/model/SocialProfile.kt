b1cb116df8df6464ac3ac622f63c8ac41e988068
suzan2go <ksuzuki180@gmail.com>
2017-08-23 00:10:53 +0900

    fix mode implementation

--- src/main/kotlin/gisty/domain/model/SocialProfile.kt
+++ src/main/kotlin/gisty/domain/model/SocialProfile.kt
@@ -1,13 +1,15 @@
 package gisty.domain.model
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.time.LocalDateTime
 
-class SocialProfile(id: Int, userId: Int, uid: String, provider: String, email: String) {
-    public var id: Int = 0
-    public var userId: Int = 0
-    public var uid: String = ""
-    public var provider: String = ""
-    public var email: String = ""
-    public var updated_datetime: LocalDateTime = LocalDateTime.now()
-    public var created_datetime: LocalDateTime = LocalDateTime.now()
+class SocialProfile(id: Int, userId: Int, uid: String, provider: String, email: String, auth: LinkedHashMap<String, Any>) {
+    var id: Int = id
+    var userId: Int = userId
+    var uid: String = uid
+    var provider: String = provider
+    var email: String = email
+    var auth: String = ObjectMapper().writeValueAsString(auth)
+    var updatedDatetime: LocalDateTime = LocalDateTime.now()
+    var createdDatetime: LocalDateTime = LocalDateTime.now()
 }
--- src/main/kotlin/gisty/domain/model/User.kt
+++ src/main/kotlin/gisty/domain/model/User.kt
@@ -3,9 +3,10 @@ package gisty.domain.model
 import java.io.Serializable
 import java.time.LocalDateTime
 
-class User(id: Int, name: String): Serializable {
-    public var id: Int = 0
-    public var name: String = ""
-    public var updated_datetime: LocalDateTime = LocalDateTime.now()
-    public var created_datetime: LocalDateTime = LocalDateTime.now()
+class User(id: Int, name: String, email: String): Serializable {
+    var id: Int = id
+    var name: String = name
+    var email: String = email
+    var updatedDatetime: LocalDateTime = LocalDateTime.now()
+    var createdDatetime: LocalDateTime = LocalDateTime.now()
 }
