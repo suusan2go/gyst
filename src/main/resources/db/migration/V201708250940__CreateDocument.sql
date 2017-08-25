c13332b348b299697a74747e606710d26cc3354d
suzan2go <ksuzuki180@gmail.com>
2017-08-25 21:46:39 +0900

    add document schema

---
+++ src/main/resources/db/migration/V201708250940__CreateDocument.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS documents(
+  id SERIAL PRIMARY KEY,
+  user_id integer NOT NULL REFERENCES users ON DELETE CASCADE,
+  title VARCHAR(128) NOT NULL,
+  body text NOT NULL,
+  updated_datetime TIMESTAMP NOT NULL,
+  created_datetime TIMESTAMP NOT NULL,
+)
