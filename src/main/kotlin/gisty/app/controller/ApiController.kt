20f74f640a88e9821c8ed5074a3a06e77a1db9e8
suzan2go <ksuzuki180@gmail.com>
2017-08-26 00:59:42 +0900

    create base api controller and mapping default api prefix

---
+++ src/main/kotlin/gisty/app/controller/ApiController.kt
@@ -0,0 +1,9 @@
+package gisty.app.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+open class ApiController {
+}
--- src/main/kotlin/gisty/app/controller/user/UserController.kt
+++ src/main/kotlin/gisty/app/controller/user/UserController.kt
@@ -1,14 +1,13 @@
 package gisty.app.controller.user
 
+import gisty.app.controller.ApiController
 import gisty.domain.model.User
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.Mapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
-class UserController {
+class UserController: ApiController() {
     @RequestMapping("/user")
     fun user(@AuthenticationPrincipal principal: User) : User {
         return principal
