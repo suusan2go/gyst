2b21d2e9be0bcc8cc9f7292d3e5011c27cc719d3
suzan2go <ksuzuki180@gmail.com>
2017-08-27 20:24:35 +0900

    add exception handler for NotFoundException

---
+++ src/main/kotlin/gisty/app/controller/ExceptionHandlerController.kt
@@ -0,0 +1,26 @@
+package backend.controller.exceptions
+
+import gisty.app.exception.NotFoundException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.time.LocalDateTime
+import java.time.ZoneOffset.UTC
+
+@ControllerAdvice
+class ExceptionHandlerController {
+
+    private val logger = LoggerFactory.getLogger(ExceptionHandlerController::class.java)
+
+    // Used for handling Jackson Parsing Exceptions
+    @ExceptionHandler(NotFoundException::class)
+    fun handle(e: NotFoundException): ResponseEntity<Map<String, String>> {
+        val response = mapOf(
+                "timestamp" to LocalDateTime.now().toEpochSecond(UTC).toString(),
+                "message" to e.message!!
+        )
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    }
+}
