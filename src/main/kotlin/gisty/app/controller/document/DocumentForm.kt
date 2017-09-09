b80377a0882dfb704a8014a774f2014be4fa0450
suzan2go <ksuzuki180@gmail.com>
2017-09-09 23:11:24 +0900

    add form validation

--- src/main/kotlin/gisty/app/controller/document/DocumentForm.kt
+++ src/main/kotlin/gisty/app/controller/document/DocumentForm.kt
@@ -6,28 +6,11 @@ import java.io.Serializable;
 import java.time.LocalTime;
 
 
-class DocumentForm: Serializable{
-    @NotBlank(message = "必須です")
-    private var startTime: LocalTime? = null
-
-    @NotBlank(message = "必須です")
-    private var endTime: LocalTime? = null
-
-    fun getEndTime(): LocalTime? {
-        return endTime
-    }
-
-    fun getStartTime(): LocalTime? {
-        return startTime
-    }
-
-    fun setEndTime(endTime: LocalTime) {
-        this.endTime = endTime
-    }
-
-    fun setStartTime(startTime: LocalTime) {
-        this.startTime = startTime
-    }
-
-
+class DocumentForm: Serializable {
+    @get:NotBlank(message = "必須です")
+    var title: String? = null
+    @get:NotBlank(message = "必須です")
+    var body: String? = null
 }
+
+
