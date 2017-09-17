98496975de99f505e6e435a97a4f6ccc5266d3ad
suzan2go <ksuzuki180@gmail.com>
2017-09-17 21:20:35 +0900

    return markdown converted html

--- src/main/kotlin/gisty/domain/model/Document.kt
+++ src/main/kotlin/gisty/domain/model/Document.kt
@@ -1,5 +1,10 @@
 package gisty.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.commonmark.node.Node
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer;
 import java.time.LocalDateTime
 
 class Document: Entity {
@@ -7,6 +12,10 @@ class Document: Entity {
     var userId: Int = 0
     lateinit var title: String
     lateinit var body: String
+    @get:JsonIgnore
+    val renderer: HtmlRenderer = HtmlRenderer.builder().build()
+    @get:JsonIgnore
+    val parser: Parser = Parser.builder().build()
 
     constructor(id: Int, userId: Int, title: String, body: String): this() {
         this.id = id
@@ -15,6 +24,12 @@ class Document: Entity {
         this.body = body
         this.updatedDatetime = LocalDateTime.now()
         this.createdDatetime = LocalDateTime.now()
+    }
+
+    @JsonProperty("bodyHtml")
+    fun getBodyHtml(): String{
+        val node: Node = parser.parse(body)
+        return renderer.render(node)
     }
 
     private constructor(): super() {}
