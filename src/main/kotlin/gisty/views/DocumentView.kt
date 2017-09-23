45a9e4423c25880490b7ffda01a9e6781aac95f7
suzan2go <ksuzuki180@gmail.com>
2017-09-24 00:36:39 +0900

    create view class

---
+++ src/main/kotlin/gisty/views/DocumentView.kt
@@ -0,0 +1,21 @@
+package gisty.views
+
+import gisty.domains.document.Document
+
+data class DocumentView(
+        val id: Int,
+        val title: String,
+        val body: String,
+        val bodyHtml: String
+){
+    companion object {
+        operator fun invoke(document: Document): DocumentView{
+            return DocumentView(
+                document.id.value,
+                document.title.value,
+                document.body.value,
+                document.body.html()
+            )
+        }
+    }
+}
